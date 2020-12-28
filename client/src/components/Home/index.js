0df954041128b510208f25bb0b9d84e69cfafabf
Jevon Cochran <jevon.cochran@gmail.com>
2020-12-27 18:26:54 -0800

    Home Inquiry

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Intro from "../Intro";
+import Inquiry from "./Inquiry";
 
 const Home = () => {
   return (
     <div>
       <Intro />
+      <Inquiry />
     </div>
   );
 };
