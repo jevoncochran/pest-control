a028769903dd6da855c1e0e231793dc662da3576
Jevon Cochran <jevon.cochran@gmail.com>
2020-12-29 00:24:51 -0800

    hamburger menu for Nav mobile

--- client/src/components/Nav/Bottom.js
+++ client/src/components/Nav/Bottom.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.scss";
 import { NavLink } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
+import MobileMenu from "./MobileMenu";
 
 const Bottom = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const openDrawer = () => {
+    setDrawerOpen(true);
+  };
+
+  const closeDrawer = () => {
+    setDrawerOpen(false);
+  };
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  useEffect(() => {
+    console.log("drawerOpen: ", drawerOpen);
+  }, [drawerOpen]);
+
   return (
     <div className="nav-bottom">
       <div className="nav-bottom-logo-container">
@@ -26,9 +45,16 @@ const Bottom = () => {
           CONTACT
         </NavLink>
       </div>
-      <div className="nav-bottom-hamburger-icon">
+      <div className="nav-bottom-hamburger-icon" onClick={toggleDrawer}>
         <GiHamburgerMenu />
       </div>
+      <MobileMenu
+        className="nav-mobile-menu"
+        openDrawer={openDrawer}
+        closeDrawer={closeDrawer}
+        drawerOpen={drawerOpen}
+        style={{ width: "80%", border: "2px solid green" }}
+      />
     </div>
   );
 };
